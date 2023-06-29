f7ce6e8007ed9166620665c0cd5e3372f9443972
hpalermoemerick <124798752+hpalermoemerick@users.noreply.github.com>
2023-06-29 14:35:38 -0300

    Update atv009.java

--- Atividades Java/Atv009/atv009.java
+++ Atividades Java/Atv009/atv009.java
@@ -2,26 +2,28 @@ public class ExercicioSemRetorno {
     
 
     public static void main(String[] args) {
-        Integer[] vetorA = {1,2,3,4,5,6,7,8,9,10}; // Cria o vetor A
-        Integer[] vetorB = {10,20,30,22,33,24,32,12,43,54}; // Cria o vetor B
-        Integer[] vetorC = new Integer[10]; // Cria o vetor C
-        imprimirNumeros(vetorA); // Executa funcao imprimirNumeros
-        SomaNumeros(vetorA,vetorB,vetorC);// Executa funcao SomaNumeros
-        imprimirNumeros(vetorC);// Executa funcao imprimirNumeros
-    }
+        // cria os vetores
+        Integer[] vetorA = {1,2,3,4,5,6,7,8,9,10};
+        Integer[] vetorB = {10,20,30,22,33,24,32,12,43,54}; 
+        Integer[] vetorC = new Integer[10];
+        imprimirNumeros(vetorA); 
+        SomaNumeros(vetorA,vetorB,vetorC); SomaNumeros
+        imprimirNumeros(vetorC);
 
     public static void imprimirNumeros(Integer[] vetor){
-        for (int i = 0; i < vetor.length; i++) { // Estrutura de repeticao para ler o vetor
-            System.out.print(vetor[i] + " "); // Imprimir o valor de cada posicao do vetor
+        for (int i = 0; i < vetor.length; i++) { 
+            System.out.print(vetor[i] + " ");
         }
-        System.out.print("\n"); // Pular uma linha
+        System.out.print("\n"); 
     }
 
     public static void SomaNumeros(Integer[] vetorA, Integer[] vetorB, Integer[] vetorC){
-        int soma = 0; // Cria variavel soma
-        for (int i = 0; i < vetorB.length; i++) { // Estrutura de repeticao para ler o vetor
-            soma = vetorB[i] + vetorA[i]; // Soma os elementos do vetorA com o do vetorB
-            vetorC[i] = soma; // Adiciona o valor somando ao vetorC
+        // Cria a variavel soma
+        int soma = 0; 
+        // soma os valores e atribui-os em C
+        for (int i = 0; i < vetorB.length; i++) { 
+            soma = vetorB[i] + vetorA[i];
+            vetorC[i] = soma;
             
         }
     }
